a92e6fcd94ca46c790fa9321750b299ba69d85c9
Sajan <sajanmhatre29@gmail.com>
2021-08-20 09:20:45 +0530

    Initial commit

---
+++ src/TicTacToe.java
@@ -0,0 +1,16 @@
+
+public class TicTacToe {
+ static void tick(){
+             int nameR;
+            //Method body
+    }
+    public void main(String[] args) {
+        int [] game1 = new int[10];
+        for (int i=1; i<game1.length; i++){
+            game1[i]=0;
+
+        }
+       useCase obj =new useCase();
+
+    }
+}
